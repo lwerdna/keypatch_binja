f09f5f5f05c824a38754054605bb5a484c0ce9ec
Andrew Lamoureux <andrew@vector35.com>
2021-03-29 23:42:11 -0400

    Fail gracefully if no binaryninjaui

--- __init__.py
+++ __init__.py
@@ -1,7 +1,11 @@
-from binaryninjaui import (UIAction, UIActionHandler, Menu)
+try:
+	from binaryninjaui import (UIAction, UIActionHandler, Menu)
 
-from . import keypatch
+	from . import keypatch
 
-UIAction.registerAction("KEYPATCH\\Patcher")
-UIActionHandler.globalActions().bindAction("KEYPATCH\\Patcher", UIAction(keypatch.launch_patcher))
-Menu.mainMenu("Tools").addAction("KEYPATCH\\Patcher", "Patcher")
+	UIAction.registerAction("KEYPATCH\\Patcher")
+	UIActionHandler.globalActions().bindAction("KEYPATCH\\Patcher", UIAction(keypatch.launch_patcher))
+	Menu.mainMenu("Tools").addAction("KEYPATCH\\Patcher", "Patcher")
+except ModuleNotFoundError:
+	# probably being loaded by headless BinaryNinja
+	pass
