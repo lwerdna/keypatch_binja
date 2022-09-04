e853dba3767029edf161eb0679426ffb7ed35163
gordboy <35108003+gordboy@users.noreply.github.com>
2022-09-04 16:41:22 +0100

    Finish cosmetic code formatting

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 try:
-	from binaryninjaui import (UIAction, UIActionHandler, Menu)
+    from binaryninjaui import (UIAction, UIActionHandler, Menu)
 
-	from . import keypatch
+    from . import keypatch
 
-	UIAction.registerAction("Keypatch")
-	UIActionHandler.globalActions().bindAction("Keypatch", UIAction(keypatch.launch_keypatch))
-	Menu.mainMenu("Tools").addAction("Keypatch", "Keypatch")
+    UIAction.registerAction("Keypatch")
+    UIActionHandler.globalActions().bindAction("Keypatch", UIAction(keypatch.launch_keypatch))
+    Menu.mainMenu("Tools").addAction("Keypatch", "Keypatch")
 except ModuleNotFoundError:
-	# probably being loaded by headless BinaryNinja
-	pass
+    # probably being loaded by headless BinaryNinja
+    pass
